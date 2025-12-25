326da81bd17f786afa7f77c8926252275f72c9e9
Pybounce <pybounce@protonmail.com>
2025-12-25 16:04:22 +0000

    supporting new step api

--- src/wasm_vm.rs
+++ src/wasm_vm.rs
@@ -1,11 +1,12 @@
 use bytecode_vm::{NativeFunction, Value};
+use js_sys::Array;
 use wasm_bindgen::prelude::*;
 use bytecode_vm::interpreter::Interpreter;
-use bytecode_vm::interpreter::{InterpretResult, CompilerError, RuntimeError};
+use bytecode_vm::interpreter::{CompilerError, RuntimeError};
 
 #[wasm_bindgen]
 pub struct WasmVm {
-    natives: Vec<JsNativeFn>
+    interpreter: Interpreter
 }
 
 #[wasm_bindgen]
@@ -24,26 +25,77 @@ pub struct JsNativeFn {
 }
 
 #[wasm_bindgen]
-pub struct Output {
+impl JsNativeFn {
+    #[wasm_bindgen(constructor)]
+    pub fn new(name: String, arity: u8, function: js_sys::Function) -> JsNativeFn
+    {
+        return JsNativeFn {
+            name,
+            arity,
+            function
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct CompileResult {
     success: bool,
-    compile_errors: Vec<CompilerErr>,
-    runtime_error: String
+    vm: Option<WasmVm>,
+    compile_errors: Option<Vec<CompilerErr>>,
+}
+
+#[wasm_bindgen]
+impl CompileResult {
+    #[wasm_bindgen(getter)]
+    pub fn success(&self) -> bool {
+        self.success
+    }
+    #[wasm_bindgen]
+    pub fn take_interpreter(&mut self) -> Option<WasmVm> {
+        return self.vm.take();
+    }
+    #[wasm_bindgen]
+    pub fn take_compile_errors(&mut self) -> Option<Vec<CompilerErr>> {
+        self.compile_errors.take()
+    }
+}
+
+impl CompileResult {
+    fn new_success(interpreter: Interpreter) -> Self {
+        Self {
+            success: true,
+            vm: Some(WasmVm { interpreter }),
+            compile_errors: None,
+        }
+    }
+    fn new_failure(errors: Vec<CompilerError>) -> Self {
+        Self {
+            success: false,
+            vm: None,
+            compile_errors: Some(errors.iter().map(|i| { CompilerErr {
+                line: i.line,
+                start: i.start,
+                len: i.len
+            }}).collect()),
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub struct Output {
+    finished: bool,
+    runtime_error: Option<String>
 }
 
 #[wasm_bindgen]
 impl Output {
     #[wasm_bindgen(getter)]
-    pub fn success(&self) -> bool {
-        self.success
+    pub fn finished(&self) -> bool {
+        self.finished
     }
 
     #[wasm_bindgen(getter)]
-    pub fn compile_errors(&self) -> Vec<CompilerErr> {
-        self.compile_errors.clone()
-    }
-
-    #[wasm_bindgen(getter)]
-    pub fn runtime_error(&self) -> String {
+    pub fn runtime_error(&self) -> Option<String> {
         self.runtime_error.clone()
     }
 }
@@ -51,77 +103,117 @@ impl Output {
 impl Output {
     pub fn successful() -> Self {
         return Self {
-            success: true,
-            compile_errors: vec![],
-            runtime_error: "".to_owned()
+            finished: true,
+            runtime_error: None
         };
     }
     pub fn runtime_err(err: RuntimeError) -> Self {
         return Self {
-            success: false,
-            compile_errors: vec![],
-            runtime_error: err.message.to_owned()
+            finished: true,
+            runtime_error: Some(err.message.to_owned())
         };
     }
-    pub fn compile_err(errors: Vec<CompilerError>) -> Self {
+    pub fn unfinished() -> Self {
         return Self {
-            success: false,
-            compile_errors: errors.iter().map(|i| { CompilerErr {
-                line: i.line,
-                start: i.start,
-                len: i.len
-            }}).collect(),
-            runtime_error: "".to_owned()
-        }
+            finished: false,
+            runtime_error: None
+        };
     }
 }
 
 #[wasm_bindgen]
+pub fn compile(source: &str, natives: Vec<JsNativeFn>) -> CompileResult {
+    let mut rust_natives: Vec::<NativeFunction> = vec![];
+    for native in natives.into_iter() {
+        rust_natives.push(native.into_native());
+    }
+    return match Interpreter::new(source.to_owned(), rust_natives) {
+        Ok(interpreter) => CompileResult::new_success(interpreter),
+        Err(compiler_errors) => CompileResult::new_failure(compiler_errors),
+    };
+}
+
+#[wasm_bindgen]
 impl WasmVm {
-    #[wasm_bindgen(constructor)]
-    pub fn new() -> WasmVm {
-        WasmVm { natives: vec![] }
+    #[wasm_bindgen]
+    pub fn interpret(&mut self) -> Output {
+        return match self.interpreter.run() {
+            Ok(_) => Output::successful(),
+            Err(runtime_error) => Output::runtime_err(runtime_error),
+        };
     }
     #[wasm_bindgen]
-    pub fn interpret(&mut self, source: &str, natives: Vec<JsNativeFn>) -> Output {
-        let mut interpreter = Interpreter::new(source.to_owned());
-
-        
-        let mut rust_natives: Vec::<NativeFunction> = vec![];
-        for (i, native) in natives.into_iter().enumerate() {
-            
-            self.natives.push(native);
-
-            rust_natives.push(NativeFunction { 
-                name: native.name.clone(), 
-                arity: native.arity, 
-                function: {
-                    fn my_func(vals: &[Value]) -> Value {
-                        return self.natives[i].function.call0(&JsValue::NULL);
-                    }
-                    my_func
-                } 
-            });
-        }
-
-
-        let print = NativeFunction { 
-            name: "print".to_owned(), 
-            arity: 1, 
-            function: {
-                fn print(vals: &[Value]) -> Value {
-                    println!("{}", vals[0]);
-                    return Value::Null;
-                }
-                print            
-            }
-        };
-
-        return match interpreter.interpret(vec![print]) {
-            InterpretResult::Ok => Output::successful(),
-            InterpretResult::CompileErr(compiler_errors) => Output::compile_err(compiler_errors),
-            InterpretResult::RuntimeErr(runtime_error) => Output::runtime_err(runtime_error),        
+    pub fn step(&mut self) -> Output {
+        return match self.interpreter.step() {
+            Ok(not_finished) => {
+                if not_finished { Output::unfinished() }
+                else { Output::successful() }
+            },
+            Err(runtime_error) => Output::runtime_err(runtime_error),
         };
     }
-
 }
+
+pub trait IntoNative { 
+    fn into_native(self) -> NativeFunction; 
+}
+
+pub trait JsConvert { 
+    fn to_js(&self) -> JsValue; 
+    fn from_js(js: JsValue) -> Self; 
+}
+
+impl IntoNative for JsNativeFn {
+    fn into_native(self) -> NativeFunction {
+        let js_func = self.function;
+
+        NativeFunction {
+            name: self.name,
+            arity: self.arity,
+            function: Box::new(move |vals: &[Value]| {
+
+                let js_args = vals.iter()
+                    .map(|v| v.to_js())
+                    .collect::<Vec<_>>();
+
+                let array = Array::new();
+                for arg in js_args {
+                    array.push(&arg);
+                }
+
+                let result = js_func
+                    .apply(&JsValue::NULL, &array)
+                    .expect("JS function threw");
+
+                Value::from_js(result)
+            }),
+        }
+    }
+}
+
+impl JsConvert for Value {
+    fn to_js(&self) -> JsValue {
+        match self {
+            Value::Number(n) => JsValue::from_f64(*n),
+            Value::Bool(b) => JsValue::from_bool(*b),
+            Value::String(s) => JsValue::from_str(s),
+            Value::Null => JsValue::NULL,
+            _ => panic!("Unsupported value.")
+        }
+    }
+
+    fn from_js(js: JsValue) -> Self {
+        if js.is_null() || js.is_undefined() {
+            Value::Null
+        } else if let Some(n) = js.as_f64() {
+            Value::Number(n)
+        } else if let Some(b) = js.as_bool() {
+            Value::Bool(b)
+        } else if let Some(s) = js.as_string() {
+            Value::String(s.into())
+        } else {
+            panic!("Unsupported JS value")
+        }
+    }
+}
+
